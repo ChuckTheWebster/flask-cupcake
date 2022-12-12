968d00892665e7bd34072f993d26f2fd4af76b06
Diane <hello@dianenguyen.ca>
2022-12-12 13:22:37 -0800

    Create API routes for cupcakes

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 """Flask app for Cupcakes"""
 
-from flask import Flask, jsonify
-
 # from flask_debugtoolbar import DebugToolbarExtension
-
+from flask import Flask, jsonify, request
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
@@ -15,7 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-db.create_all()
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -23,3 +20,40 @@ db.create_all()
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 # toolbar = DebugToolbarExtension(app)
+
+@app.get("/api/cupcakes")
+def get_all_cupcakes():
+    """Returns data about all cupcakes"""
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake(cupcake_id):
+    """Gets data on one cupcake"""
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    """Creates a cupcake with flavor, size, rating, and image from body of req"""
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"] or None
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
+
--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
 
 class Cupcake(db.Model):
-    """"""
+    """Cupcake model"""
 
     __tablename__ = "cupcakes"
 
@@ -17,6 +17,15 @@ class Cupcake(db.Model):
     rating = db.Column(db.Integer, nullable=False)
     image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
 
+    def serialize(self):
+        """Serialize to dictionary"""
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image,
+        }
 
 def connect_db(app):
     """Connect this database to provided Flask app.
